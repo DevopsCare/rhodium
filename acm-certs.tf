7ad73cb1c56df6060dd32130c20b989fe3a4493e
vkropotko <vjkropotko@gmail.com>
2020-02-12 19:02:50 +0200

    Fixes

--- acm-certs.tf
+++ acm-certs.tf
@@ -8,16 +8,16 @@ resource "aws_acm_certificate" "rhodium" {
 resource "aws_route53_record" "cert_validation" {
   count = var.enable_apigw_domain ? 1 : 0
 
-  name    = aws_acm_certificate.rhodium.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.rhodium.domain_validation_options[0].resource_record_type
+  name    = aws_acm_certificate.rhodium[0].domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.rhodium[0].domain_validation_options[0].resource_record_type
   zone_id = data.aws_route53_zone.domain.id
-  records = [aws_acm_certificate.rhodium.domain_validation_options[0].resource_record_value]
+  records = [aws_acm_certificate.rhodium[0].domain_validation_options[0].resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "api" {
   count = var.enable_apigw_domain ? 1 : 0
 
-  certificate_arn         = aws_acm_certificate.rhodium.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  certificate_arn         = aws_acm_certificate.rhodium[0].arn
+  validation_record_fqdns = [aws_route53_record.cert_validation[0].fqdn]
 }
