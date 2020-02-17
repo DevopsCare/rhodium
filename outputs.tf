f783e64d2a115e52def5d431a5c7d8c23ae1dfd9
vkropotko <vjkropotko@gmail.com>
2020-02-17 18:18:24 +0200

    Fix output

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "worker_role_arn" {
 }
 
 output slack_events_api_callack {
-  value = var.enable_apigw_domain ? aws_api_gateway_domain_name.rhodium[0].domain_name : aws_api_gateway_deployment.current.invoke_url
+  value = var.enable_apigw_domain ? "https://${aws_api_gateway_domain_name.rhodium[0].domain_name}/" : aws_api_gateway_deployment.current.invoke_url
 }
